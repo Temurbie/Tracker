2441351218743757632b23d7bd4f60a62d93c6a3
temurbie <temurbie@gmail.com>
2026-02-13 23:33:03 +0500

    start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Main } from './features/layout/main/main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"", pathMatch:'full', component:Main
+    }
+];
